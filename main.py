370de4179a33919fb7fa8a199b1ad4ac2bcd3443
adideutsch <adideutsch1@gmail.com>
2019-12-09 09:21:32 +0200

    add main loop

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ from pms_app import app
 
 if __name__ == '__main__':
     create_tables(get_db_engine())
-    app.run(host='0.0.0.0', port=80)
+    while True:
+        try:
+            app.run(host='0.0.0.0', port=80)
+        except Exception as e:
+            print(f"Encountered error: {e}")
